a14b6584ccbe05cdd14bb23faed9dac09131b518
DadsRavioli <75680331+DadsRavioli@users.noreply.github.com>
2021-01-04 15:35:58 +0000

    Add files via upload
    Comments updated and added to, instructions added at the top of program.

--- Visualization.m
+++ Visualization.m
@@ -1,3 +1,6 @@
+%This software requires the installing of "Mapping Toolbox" addon, this can
+%be installed automatically through the matlab interface.
+
 global currentHour;
 global play;
 global dataSource;
@@ -16,7 +19,7 @@ box = uibuttongroup(fig2,'Position',[90 60 120 85]);
 nButton = uiradiobutton(box, 'Position', [5 60 120 15], 'Text', 'CBE Data');
 
 %Creates the next button in the UI
-%line 23 triggers the next function when it detects the button has been
+%line 27 triggers the next function when it detects the button has been
 %pushed
 nextBtn = uibutton(fig2,"push",...
     "Text", "Next",...
@@ -37,22 +40,8 @@ function startFunc()
         "Position", [90 550 595 30],...
         "Text", text);
     
-    loop(fig, n) 
+    cycleThru(fig, n) 
 end
-
-function loop(fig, n)  
-    global currentHour
-    
-    while getPlay()      
-        displayHour(currentHour, fig, n); %Shows the data depending on the current hour
-        if getHour() == getendTime()   
-            currentHour = 1;
-        else
-            currentHour = currentHour+1;              
-        end
-        pause(0.7); %Creates a 0.7 second pause between changing hours
-    end
-end             
 
 function displayHour(currentHour, fig, n)
 [x] = 30.15:0.1:69.85; % create x value
@@ -60,7 +49,7 @@ function displayHour(currentHour, fig, n)
    
 [x, y] = meshgrid(x, y); %Create a grid with the lat and long
 
-%Lines 65 to 74 taken from
+%Lines 54 to 64 taken from
 %https://cumoodle.coventry.ac.uk/pluginfile.php/3233614/mod_resource/content/2/VisualinationExample.m
 map = worldmap('Europe');
 land = shaperead('landareas', 'UseGeoCoords', true);
@@ -77,6 +66,20 @@ surfm(x, y, n(:,:,currentHour),'FaceAlpha', 0.5);%Plots the map with the data
 mapCopy = copyobj(map, fig);  
 close();                  
 end
+
+function cycleThru(fig, n)  
+    global currentHour
+    
+    while getPlay()      
+        displayHour(currentHour, fig, n); %Shows the data depending on the current hour
+        if getHour() == getendTime()   
+            currentHour = 1;
+        else
+            currentHour = currentHour+1;              
+        end
+        pause(0.7); %Creates a 0.7 second pause between changing hours
+    end
+end             
 
 function next(nButton, fig)
     if nButton.Value == 1
